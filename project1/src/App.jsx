cd467e6b120c1183de569f2ba4e70cf2598fb185
xonex2 <ankitsinghrajput170366@gmail.com>
2026-01-03 10:28:12 +0530

    nodejs

---
+++ project1/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App2.css'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className='abc'>
+      <p>
+        2500030949-Ankit raj 
+      </p>
+          </div>
+    
+  )
+}
+
+export default App
---
+++ project2/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+   
+      <div>
+        <h1>2500030949 - Ankit Raj</h1>
+        <h2>Welcome to ReactJS</h2>
+        <h3>Using Vite as a Bundler</h3>
+        </div>
+  
+  )
+}
+
+export default App
